539b01d027458bfc3296c6d6bd19f810f7c8a347
Colin Tracy <colin@aiselng.com>
2019-05-20 16:39:31 -0600

    remove gitignore files

--- node_modules/@material-ui/pickers/DatePicker/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { DatePickerProps } from './DatePicker';
-import { KeyboardDatePickerProps } from './KeyboardDatePicker';
-export declare type DatePickerProps = DatePickerProps;
-export declare type KeyboardDatePickerProps = KeyboardDatePickerProps;
-export { KeyboardDatePicker } from './KeyboardDatePicker';
-export { default } from './DatePicker';
--- node_modules/@material-ui/pickers/DateTimePicker/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var KeyboardDateTimePicker_1 = require("./KeyboardDateTimePicker");
-exports.KeyboardDateTimePicker = KeyboardDateTimePicker_1.KeyboardDateTimePicker;
-var DateTimePicker_1 = require("./DateTimePicker");
-exports.default = DateTimePicker_1.default;
